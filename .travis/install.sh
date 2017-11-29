a2ded4136e5ffe8a0c5a62604dcd585317fd5328
Adam Babik <a.babik@designfortress.com>
2017-11-29 21:44:00 +0100

    add sudo in install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,12 +3,12 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     wget https://redirector.gvt1.com/edgedl/go/go1.9.2.darwin-amd64.tar.gz
-    tar -C /usr/local -xzf go1.9.2.darwin-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go1.9.2.darwin-amd64.tar.gz
 
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 
     wget https://redirector.gvt1.com/edgedl/go/go1.9.2.linux-amd64.tar.gz
-    tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz
 
 fi
 
